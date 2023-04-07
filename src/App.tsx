00d53d0070af903fa8232e488237313079ffab30
Neil West <nswest23@gmail.com>
2023-04-07 14:34:03 -0400

    chore: linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
 // import { orderByChild, query, ref } from "firebase/database";
 // import { useDatabase, useDatabaseListData } from "reactfire";
 import { supabase } from "./supabase";
-import type { AuthSession } from '@supabase/supabase-js'
-import Auth from './components/Auth';
+import type { AuthSession } from "@supabase/supabase-js";
+import Auth from "./components/Auth";
 import { AddBookmarkModal, BookmarkList, Search } from "./components";
 import { SupabaseContext } from "./supabaseContext";
 
@@ -45,49 +45,52 @@ function App() {
 
   return (
     <div className="App">
-    {!session ? <Auth /> : 
-    <div>
-      <header>
-        <title>Bookmarks</title>
-        <meta name="description" content="bookmarking app" />
-        <link rel="icon" href="/favicon.ico" />
-      </header>
-      <AppBar
-        className={styles.appBarCustom}
-        sx={{
-          backgroundColor: "black",
-          position: "sticky",
-          top: "0",
-          py: "10px",
-        }}
-      >
-        <Grid2 container spacing={2}>
-          <Grid2 xs={8}>
-            <div className={styles.appBarActions}>
-              <AddBookmarkModal />
-            </div>
-            <div className={styles.appbarContents}>
-              <h2>Bookmarks</h2>
-              {/* <Search filterLinks={} /> */}
-            </div>
+      {!session ? (
+        <Auth />
+      ) : (
+        <div>
+          <header>
+            <title>Bookmarks</title>
+            <meta name="description" content="bookmarking app" />
+            <link rel="icon" href="/favicon.ico" />
+          </header>
+          <AppBar
+            className={styles.appBarCustom}
+            sx={{
+              backgroundColor: "black",
+              position: "sticky",
+              top: "0",
+              py: "10px",
+            }}
+          >
+            <Grid2 container spacing={2}>
+              <Grid2 xs={8}>
+                <div className={styles.appBarActions}>
+                  <AddBookmarkModal />
+                </div>
+                <div className={styles.appbarContents}>
+                  <h2>Bookmarks</h2>
+                  {/* <Search filterLinks={} /> */}
+                </div>
+              </Grid2>
+              <Grid2 sx={{ display: "flex", alignItems: "flex-end" }}>
+                <div className={styles.appbarContentsMinor}>
+                  <h4 style={{ marginBottom: "0px" }}>Tags</h4>
+                </div>
+              </Grid2>
+            </Grid2>
+          </AppBar>
+          <Grid2 container spacing={2}>
+            <Grid2 xs={8}>
+              <BookmarkList />
+            </Grid2>
+            <Grid2 xs={4}></Grid2>
+            <Grid2 xs={12}>
+              <footer className={styles.footer}>footer</footer>
+            </Grid2>
           </Grid2>
-          <Grid2 sx={{ display: "flex", alignItems: "flex-end" }}>
-            <div className={styles.appbarContentsMinor}>
-              <h4 style={{ marginBottom: "0px" }}>Tags</h4>
-            </div>
-          </Grid2>
-        </Grid2>
-      </AppBar>
-      <Grid2 container spacing={2}>
-        <Grid2 xs={8}>
-          <BookmarkList />
-        </Grid2>
-        <Grid2 xs={4}></Grid2>
-        <Grid2 xs={12}>
-          <footer className={styles.footer}>footer</footer>
-        </Grid2>
-      </Grid2>
-      </div>}
+        </div>
+      )}
     </div>
   );
 }
